c6d2c368a7bacf7c158ac7a0de7f038df573782e
darianthao <justdarian01@gmail.com>
2023-02-08 21:42:51 -0600

    installed dependencies, got a response, pokemon name

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,57 @@
 package org.example;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.json.simple.parser.JSONParser;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            //Public API:
+            //https://www.metaweather.com/api/location/search/?query=<CITY>
+            //https://www.metaweather.com/api/location/44418/
+
+            URL url = new URL("https://pokeapi.co/api/v2/pokemon/1");
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+
+            //Check if connect is made
+            int responseCode = conn.getResponseCode();
+
+            // 200 OK
+            if (responseCode != 200) {
+                throw new RuntimeException("HttpResponseCode: " + responseCode);
+            } else {
+
+                StringBuilder informationString = new StringBuilder();
+                Scanner scanner = new Scanner(url.openStream());
+
+                while (scanner.hasNext()) {
+                    informationString.append(scanner.nextLine());
+                }
+                //Close the scanner
+                scanner.close();
+
+          //      System.out.println(informationString);
+
+                //JSON simple library Setup with Maven is used to convert strings to JSON
+                JSONParser parse = new JSONParser();
+                JSONObject pokemonObject = (JSONObject) parse.parse(String.valueOf(informationString));
+
+                String pokemonName = pokemonObject.get("name").toString();
+                System.out.println(pokemonName);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-}
+
+
+
+    }
